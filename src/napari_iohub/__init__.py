584f6e4e5829d73df83fbc8ffec6f1cf8d36cd34
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-07-22 10:59:59 -0700

    fix exports

--- src/napari_iohub/__init__.py
+++ src/napari_iohub/__init__.py
@@ -5,8 +5,15 @@ import os
 
 from ._reader import napari_get_reader
 from ._widget import MainWidget
+from ._edit_labels import EditLabelsWidget
+from ._view_tracks import open_image_and_tracks
 
-__all__ = ("napari_get_reader", "MainWidget", "example_magic_widget")
+__all__ = (
+    "napari_get_reader",
+    "MainWidget",
+    "EditLabelsWidget",
+    "open_image_and_tracks",
+)
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(os.getenv("NAPARI_IOHUB_LOGGING_LEVEL", logging.DEBUG))
