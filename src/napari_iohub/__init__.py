b6c63365abde18e59377a6e5e02e110e95c70586
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-07-22 10:58:42 -0700

    format

--- src/napari_iohub/__init__.py
+++ src/napari_iohub/__init__.py
@@ -6,11 +6,7 @@ import os
 from ._reader import napari_get_reader
 from ._widget import MainWidget
 
-__all__ = (
-    "napari_get_reader",
-    "MainWidget",
-    "example_magic_widget",
-)
+__all__ = ("napari_get_reader", "MainWidget", "example_magic_widget")
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(os.getenv("NAPARI_IOHUB_LOGGING_LEVEL", logging.DEBUG))
