6955b76b104e37a3ac605bba669d54fdf8dcb0b9
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2024-12-24 00:08:51 -0500

    Refactor __init__ to skip `open_image_and_tracks` if ultrack is not installed

--- src/napari_iohub/__init__.py
+++ src/napari_iohub/__init__.py
@@ -3,15 +3,20 @@ import os
 
 from ._edit_labels import EditLabelsWidget
 from ._reader import napari_get_reader
-from ._view_tracks import open_image_and_tracks
 from ._widget import MainWidget
 
 __all__ = (
     "napari_get_reader",
     "MainWidget",
     "EditLabelsWidget",
-    "open_image_and_tracks",
 )
+
+try:
+    from ._view_tracks import open_image_and_tracks
+    __all__ += ("open_image_and_tracks",)
+except ImportError:
+    # napari-iohub[clustering] is not installed
+    pass
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(os.getenv("NAPARI_IOHUB_LOGGING_LEVEL", logging.DEBUG))
