dfce56e71d370769a559e28dd40d0487a2190ead
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2025-04-16 10:21:25 -0700

    format

--- src/napari_iohub/_tests/test_view_tracks.py
+++ src/napari_iohub/_tests/test_view_tracks.py
@@ -1,3 +1,5 @@
 def test_view_tracks_widget(make_napari_viewer):
     viewer = make_napari_viewer()
-    _ = viewer.window.add_plugin_dock_widget("napari-iohub", "Single-cell features")
+    _ = viewer.window.add_plugin_dock_widget(
+        "napari-iohub", "Single-cell features"
+    )
